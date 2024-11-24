2869d043bbad438379be01c3cca37b2128a90b1c
mdsaj <mdsajjadhossain614@gmail.com>
2024-11-24 19:49:10 +0600

    new commit

--- src/components/API/apiClient.js
+++ src/components/API/apiClient.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an Axios instance with the base URL
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    //baseURL: "http://localhost:8080/api",
+    baseURL: "https://fashion-e-commerce-backend.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
